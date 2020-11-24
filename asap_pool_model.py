23ea4ee3d1c750c070e8438d80588aec2420b6f0
Fanjin Zhang <zfjsail@163.com>
2020-11-24 16:25:31 +0800

    change data

--- asap_pool_model.py
+++ asap_pool_model.py
@@ -6,15 +6,17 @@ from torch_scatter import scatter_mean, scatter_max
 from asap_pool import ASAP_Pooling
 import pdb
 
+
 def readout(x, batch):
     x_mean = scatter_mean(x, batch, dim=0)
-    x_max, _ = scatter_max(x, batch, dim=0) 
+    x_max, _ = scatter_max(x, batch, dim=0)
     return torch.cat((x_mean, x_max), dim=-1)
+
 
 class ASAP_Pool(torch.nn.Module):
     def __init__(self, dataset, num_layers, hidden, ratio=0.8, **kwargs):
         super(ASAP_Pool, self).__init__()
-        if type(ratio)!=list:
+        if type(ratio) != list:
             ratio = [ratio for i in range(num_layers)]
         self.conv1 = GCNConv(dataset.num_features, hidden)
         self.pool1 = ASAP_Pooling(in_channels=hidden, ratio=ratio[0], **kwargs)
@@ -23,7 +25,7 @@ class ASAP_Pool(torch.nn.Module):
         for i in range(num_layers - 1):
             self.convs.append(GCNConv(hidden, hidden))
             self.pools.append(ASAP_Pooling(in_channels=hidden, ratio=ratio[i], **kwargs))
-        self.lin1 = Linear(2*hidden, hidden) # 2*hidden due to readout layer
+        self.lin1 = Linear(2 * hidden, hidden)  # 2*hidden due to readout layer
         self.lin2 = Linear(hidden, dataset.num_classes)
         self.reset_parameters()
 
@@ -43,7 +45,8 @@ class ASAP_Pool(torch.nn.Module):
         xs = readout(x, batch)
         for conv, pool in zip(self.convs, self.pools):
             x = F.relu(conv(x=x, edge_index=edge_index, edge_weight=edge_weight))
-            x, edge_index, edge_weight, batch, perm = pool(x=x, edge_index=edge_index, edge_weight=edge_weight, batch=batch)
+            x, edge_index, edge_weight, batch, perm = pool(x=x, edge_index=edge_index, edge_weight=edge_weight,
+                                                           batch=batch)
             xs += readout(x, batch)
         x = F.relu(self.lin1(xs))
         x = F.dropout(x, p=0.5, training=self.training)
